91b208edb650c909444c7a2d73b7dfcfb2f95776
Nikwigize3 <nikwigizediouf@gmail.com>
2024-11-22 22:54:41 +0200

    Weather-app

--- html/script.js
+++ html/script.js
@@ -1,26 +1,50 @@
-const cityInput = document.querySelector(".city")
+const cityInput = document.querySelector(".city-input")
 const searchBtn = document.querySelector(".search-btn")
 
+const weatherInfoSection = document.querySelector(".weatherinfo")
+ const notfoundSection = document.querySelector('.not-found')
+ const searchCitySection = document.querySelector(".search-city")
+
+const apiKey = '64dd50eaf59471521b81b03669904637'
+
 searchBtn.addEventListener('click', () => {
-  if (cityInput.value. trim()!= '')  {
-    updateweatherinfo(cityInput.value)
+  if (cityInput.value.trim() != '')  {
+    updateWeatherInfo(cityInput.value)
     cityInput.value = ''
     cityInput.blur()
   }
 })
 cityInput.addEventListener('keydown', (event) => {
   if(event.key == 'enter' && 
-    cityInput.value. trim()!= ""
+    cityInput.value.trim() != ''
   ){
-    updateweatherinfo(cityInput.value )
-    cityInput.value = ""
+    updateWeatherInfo(cityInput.value)
+    cityInput.value = ''
     cityInput.blur()
   }
 })
 
-function getfetchData() {
-  
+async function getfetchData(endPoint, city) {
+  const apiUrl = `https://api.openweathermap.org/data/2.5/${endPoint}?q=${city}&appid=${apiKey}&units=metric`;
+
+
+const respone = await fetch(apiUrl)
+return respone.json()
 }
-function updateweatherinfo(city) {
-  const weatherdata = getfetchData()
+async function updateWeatherInfo(city) {
+  const weatherData = getfetchData('weather', city)
+
+  if(weatherData.cod != 200) {
+    ShowDisplaySection(notfoundSection)
+    return
+  }
+
+  ShowDisplaySection(weatherInfoSection)
+}
+
+function ShowDisplaySection(section){
+  [weatherInfoSection, searchCitySection, notfoundSection]
+  .forEach(section => section.style.display = 'none'  )
+
+  section.style.display = 'flex';
 }
